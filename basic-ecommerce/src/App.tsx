50258506be64b0dd55bc2f7987b202c90bb38ebb
hakanacardev <hakanacar200@gmail.com>
2023-07-03 16:07:37 +0300

    layout create

--- basic-ecommerce/src/App.tsx
+++ basic-ecommerce/src/App.tsx
@@ -1,25 +1,96 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Battery from "./Icons/Battery.svg"
+import Wifi from "./Icons/Wifi.svg"
+import Connect from "./Icons/Connect.svg"
+import List from "./Icons/List.svg"
+import Account from "./Icons/Account.svg"
+import { Button, MenuItem, Select, TextField } from '@mui/material';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className='Layout'>
+        <div className='Header'>
+          <div className='Time'>9:41</div>
+          <div className='StatusBar'>
+            <img src={Battery} />
+            <img src={Wifi} />
+            <img src={Connect} />
+          </div>
+        </div>
+        <div className='Main'>
+          <h3>Account</h3>
+          <TextField style={{ width: '100%' }} label="E-mail" variant="standard" />
+          <TextField style={{ width: '100%' }} label="Password" variant="standard" />
+          <Select
+            labelId="demo-simple-select-label"
+            label="Locale"
+            value={"TR"}
+            style={{ width: '100%' }}
+          >
+            <MenuItem value={"TR"}>Türkçe</MenuItem>
+            <MenuItem value={"EN"}>İngilizce</MenuItem>
+          </Select>
+          <Button
+            style={{ width: '100%' }}
+            disabled
+            variant="contained">Sing Up</Button>
+
+        </div>
+
+        <div className='Footer'>
+          <div style={{ alignItems: 'center', width: '50%', justifyContent: 'center', display: 'flex', flexDirection: 'column' }}>
+            <img src={List} />
+            List
+          </div>
+          <div style={{ alignItems: 'center', width: '50%', justifyContent: 'center', display: 'flex', flexDirection: 'column' }}>
+            <img src={Account} />
+            List
+          </div>
+        </div>
+      </div>
+      <div className='Layout'>
+        <div className='Header'>
+          <div className='Time'>9:41</div>
+          <div className='StatusBar'>
+            <img src={Battery} />
+            <img src={Wifi} />
+            <img src={Connect} />
+          </div>
+        </div>
+        <div className='Main'>
+          <h3>Account</h3>
+          <TextField style={{ width: '100%' }} label="E-mail" variant="standard" />
+          <TextField style={{ width: '100%' }} label="Password" variant="standard" />
+          <Select
+            labelId="demo-simple-select-label"
+            label="Locale"
+            value={"TR"}
+            style={{ width: '100%' }}
+          >
+            <MenuItem value={"TR"}>Türkçe</MenuItem>
+            <MenuItem value={"EN"}>İngilizce</MenuItem>
+          </Select>
+          <Button
+            style={{ width: '100%' }}
+            disabled
+            variant="contained">Sing Up</Button>
+
+        </div>
+
+        <div className='Footer'>
+          <div style={{ alignItems: 'center', width: '50%', justifyContent: 'center', display: 'flex', flexDirection: 'column' }}>
+            <img src={List} />
+            List
+          </div>
+          <div style={{ alignItems: 'center', width: '50%', justifyContent: 'center', display: 'flex', flexDirection: 'column' }}>
+            <img src={Account} />
+            List
+          </div>
+        </div>
+      </div>
+    </div >
   );
 }
 
