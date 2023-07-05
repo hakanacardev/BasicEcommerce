3b029aa426356d9221b34c66f410b23251c819ea
hakanacardev <hakanacar200@gmail.com>
2023-07-05 12:23:44 +0300

    comment

--- basic-ecommerce/src/context/ItemContext.tsx
+++ basic-ecommerce/src/context/ItemContext.tsx
@@ -13,15 +13,21 @@ interface Iprops {
 }
 const ItemContext = ({ children }: Iprops) => {
     const [itemCart, setItemCart] = useState([])
-
+    //sepete ürün ekler  
     const addToCart = (item: any) => setItemCart(itemCart.find(v => v.id === item.id)
         ? itemCart.map((v: any) => v.id === item.id ? { ...v, count: v.count + 1 } : v)
         : [...itemCart, { ...item, count: 1 }]
     )
+    //sepetten ürün çıkartır
     const removeFromCart = (id: any) => setItemCart(itemCart.filter(v => v.id !== id))
+
+    //sepette ki ürünü arttırır
+
     const increase = (id: any) => {
         setItemCart(itemCart.map(v => v.id === id ? { ...v, count: v.count + 1 } : v))
     }
+    //sepetten 1 ürün çıakrtır. 1 ürün kalıncaya kadar devam eder.
+
     const decrease = (id: any) => {
         setItemCart(itemCart.map(v => v.id === id ? { ...v, count: v.count > 1 ? v.count - 1 : 1 } : v))
     }
